9895f515a0ddf8afe01aecf9b7f0212fa79e1f09
ron029 <bonifacioronmark@gmail.com>
2024-12-21 13:01:44 +0800

    config

--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
-
+import router from './router';
+import store from "./store";
+import axios from "./plugins/axios";
+import VueCookies from 'vue-cookies';
 Vue.config.productionTip = false
-
+Vue.use(VueCookies)
 new Vue({
+  router,
+  store,
   vuetify,
+  axios,
   render: h => h(App)
 }).$mount('#app')
--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,11 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  devServer: {
+    port: 8080,
+    host: '0.0.0.0',
+    hot: false,
+    liveReload: true,
+  },
 })
