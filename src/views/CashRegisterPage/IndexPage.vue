a63cea18694a1a5b084c5c8ef9656aad5d396527
ron029 <bonifacioronmark@gmail.com>
2025-02-11 22:51:57 +0800

    improve cashRegister

--- src/views/CashRegisterPage/IndexPage.vue
+++ src/views/CashRegisterPage/IndexPage.vue
@@ -1,8 +1,20 @@
 <template>
     <div style="font-weight: 600;">
         <v-row no-gutters>
-            <v-col lg="5" md="5" style="padding: 10px 5px 0 10px;">
+            <v-col lg="1" md="1" style="padding: 10px 5px 0 10px;">
+                <BtnShortcuts />
+            </v-col>
+            <v-col lg="6" md="6" style="padding: 10px 5px 0 10px;">
                 <div>
+                    <BarcodeQuantity
+                        :focusToBarcode="focus.barcode"
+                        @saveBarcodeQuantity="saveBarcodeQuantity"
+                        @offFocusToBarcode="focus.barcode = false"
+                    />
+                    <div style="border: 1px solid black; padding: 10px; margin-top: 10px; position: relative;">
+                        <p class="title1" style="margin: 0; padding: 0; position: absolute; left: 10px; top: 10px;">TOTAL</p>
+                        <p class="text-right" style="font-size: 80px; font-weight: 700; color: red; padding: 0; margin: 0">{{ totalAmount.toFixed(2) }}</p>
+                    </div>
                     <!-- <v-text-field
                         value="1002000000000205"
                         outlined
@@ -17,9 +29,9 @@
                     <div>
                         <p class="title1 colorBlue" style="margin: 15px 0 0 0;">Product Information</p>
                         <ul style="list-style-type: none; padding-left: 0;">
-                            <li>Barcode: <span class="colorBlue">{{ currentTransaction.barcode }}</span></li>
-                            <li>Description: <span class="colorBlue">{{ currentTransaction.name }}</span></li>
-                            <li>Selling Price: <span class="colorBlue">{{ currentTransaction.selling_price ? `${parseInt(currentTransaction.selling_price).toFixed(2)} each` : null }}</span></li>
+                            <li>Barcode: <span class="colorBlue">{{ currentTransaction ? currentTransaction.barcode : '' }}</span></li>
+                            <li>Description: <span class="colorBlue">{{ currentTransaction ? currentTransaction.name : '' }}</span></li>
+                            <li>Selling Price: <span class="colorBlue">{{ currentTransaction && currentTransaction.selling_price ? `${Number(currentTransaction.selling_price).toFixed(2)} each` : null }}</span></li>
                         </ul>
                     </div>
                     <!-- <div>
@@ -36,23 +48,14 @@
                         </v-row>
                     </div> -->
                     <p>Clerk: {{ cashierName }}</p>
-                    <div>
-                        <div style="height: 100px; width: 100px; background-color: blue; color: white; display: flex; justify-content: center; align-items: center;">F8 Payment</div>
-                    </div>
                 </div>
             </v-col>
-            <v-col lg="7" md="7" style="padding: 10px 10px 0 5px;">
+            <v-col lg="5" md="5" style="padding: 10px 10px 0 5px;">
                 <div>
-                    <BarcodeQuantity
-                        :focusToBarcode="focus.barcode"
-                        @saveBarcodeQuantity="saveBarcodeQuantity"
-                        @offFocusToBarcode="focus.barcode = false"
+                    <ItemTable
+                        :transactions="transactions"
+                        :tendered="tendered"
                     />
-                    <div style="border: 1px solid black; padding: 10px; margin-top: 10px; position: relative;">
-                        <p class="title1" style="margin: 0; padding: 0; position: absolute; left: 10px; top: 10px;">TOTAL</p>
-                        <p class="text-right" style="font-size: 80px; font-weight: 700; color: red; padding: 0; margin: 0">{{ totalAmount.toFixed(2) }}</p>
-                    </div>
-                    <ItemTable :transactions="transactions" />
                 </div>
             </v-col>
         </v-row>
@@ -66,6 +69,23 @@
             :data="{message: 'Something went wrong'}"
             @closeDialog="closeNotif"
         />
+        <NotifDialog
+            :show="show.cancel"
+            :data="{message: ''}"
+            @closeDialog="show.cancel=false"
+        >
+            <template slot="action">
+                <p>Cancel this transaction?</p>
+                <p>
+                    <v-btn color="error" @click="show.cancel = false">cancel
+                        <v-chip x-small style="border-radius: 3px; width: 43px; padding: 0 0 0 2px; margin-left: 2px;">Enter</v-chip>
+                    </v-btn>
+                    <v-btn color="success" @click="cancelTransaction" style="margin-left: 6px;">proceed
+                        <v-chip x-small style="border-radius: 3px; width: 18px; padding: 0 0 0 2px; margin-left: 2px;">F7</v-chip>
+                    </v-btn>
+                </p>
+            </template>
+        </NotifDialog>
         <ChangeDialog
             :show="show.change"
             :change="changeAmount"
@@ -80,6 +100,7 @@
 </template>
 
 <script>
+import BtnShortcuts from './BtnShortcuts.vue';
 import EditCurrentItem from './EditCurrentItem.vue';
 import ItemTable from './ItemTable.vue';
 import ChangeDialog from './ChangeDialog.vue';
@@ -89,6 +110,7 @@ import { mapActions, mapGetters } from 'vuex';
 import BarcodeQuantity from './BarcodeQuantity.vue';
 export default {
     components: {
+        BtnShortcuts,
         EditCurrentItem,
         ItemTable,
         ChangeDialog,
@@ -99,14 +121,16 @@ export default {
     data: () => ({
         cashierName: null,
         changeAmount: 0,
+        tendered: 0,
         edit: {
             selling_price: 0,
             quantity: 0
         },
         transactions: [],
         show: {
+            cancel: false,
             payment: false,
-            change: true,
+            change: false,
             error: false,
             editItem: false,
         },
@@ -119,10 +143,11 @@ export default {
         currentTransaction() {
             if (this.transactions.length > 0) {
                 const existingProductIndex = this.transactions.findIndex(item => item.isCurrent === true)
-                return this.transactions[existingProductIndex]
-            } else {
-                return { id: null, description: null, selling_price: null, amount: null, qty: null, unit: null, barcode: null  }
+                if (existingProductIndex !== -1) {
+                    return this.transactions[existingProductIndex]
+                }
             }
+            return { id: null, description: null, name: null, selling_price: null, amount: null, qty: null, unit: null, barcode: null }
         },
         totalAmount() {
             return this.transactions.reduce((total, transaction) => {
@@ -142,11 +167,16 @@ export default {
     },
     methods: {
         ...mapActions(['saveSales', 'getCsrfToken']),
+        cancelTransaction() {
+            this.transactions = []
+            this.show.cancel = false
+        },
         closeEditCurrentItem(data) {
             this.show.editItem = false
             console.log('closeEditCurrentItem data: ', data)
         },
         closechangeAmount() {
+            this.tendered = 0
             this.show.change = false
             this.transactions = []
         },
@@ -155,6 +185,7 @@ export default {
         },
         async saveTransaction(data) {
             await this.getCsrfToken()
+            this.tendered = data.tendered
             this.saveSales({items: this.transactions, totalAmount: this.totalAmount, ...data, register_cash_flow_id: Number(window.$cookies.get('cash_register_recorded_id'))})
         },
         focusToBarcode() {
@@ -170,24 +201,27 @@ export default {
 
             // Find the index of the existing product in transactions
             const existingProductIndex = this.transactions.findIndex(item => item.product_id === data.product_id);
-
             if (existingProductIndex !== -1) {
-                // If the product exists, update its quantity and selling_price
+                // If the product exists, update its quantity
                 const existingProduct = this.transactions[existingProductIndex];
-                existingProduct.itemQuantity += parseInt(data.itemQuantity);
-                existingProduct.selling_price = (
-                    parseFloat(existingProduct.selling_price) + parseFloat(data.selling_price)
-                ).toFixed(2); // Ensure the selling_price stays a string with 2 decimal places
-                existingProduct.isCurrent = true
-                // Remove the existing product from its current position
-                // this.transactions.splice(existingProductIndex, 1);
 
-                // Add the updated product to the beginning of the list
-                // this.transactions.unshift(existingProduct);
+                if ((existingProduct.itemQuantity + parseInt(data.itemQuantity)) > existingProduct.quantity) {
+                    console.error('no items left')
+                } else {
+                    existingProduct.itemQuantity += parseInt(data.itemQuantity);
+                    existingProduct.isCurrent = true
+                    // Remove the existing product from its current position
+                    // this.transactions.splice(existingProductIndex, 1);
+
+                    // Add the updated product to the beginning of the list
+                    // this.transactions.unshift(existingProduct);
+                }
+
             } else {
-                // Add the new product to the beginning of the list
+                // Add the new product to the ending of the list
+                data.isPrinting = false
                 data.isCurrent = true
-                this.transactions.unshift(data);
+                this.transactions.push(data);
             }
         },
         handleKeyPress(event) {
@@ -195,6 +229,13 @@ export default {
                 event.preventDefault()
                 this.closechangeAmount()
                 this.show.change = false
+            }
+            if (event.key === "F7") {
+                event.preventDefault()
+                if (this.show.cancel)
+                    this.cancelTransaction()
+                else if (this.transactions.length > 0)
+                    this.show.cancel = true
             }
             if (event.altKey && event.key === "e" || event.altKey && event.key === "E") {
                 event.preventDefault()
