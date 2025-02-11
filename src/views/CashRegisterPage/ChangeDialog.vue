f6c0c908e79b021749d6f2e520dfe402d4a76aaa
ron029 <bonifacioronmark@gmail.com>
2025-02-11 22:53:00 +0800

    remove unused id

--- src/views/CashRegisterPage/ChangeDialog.vue
+++ src/views/CashRegisterPage/ChangeDialog.vue
@@ -3,7 +3,7 @@
         v-model=showDialog
         width="400"
     >
-        <v-card style="width: 400px" id="printableArea">
+        <v-card style="width: 400px">
             <v-card-title style="background-color: blue; color: white">Change Info</v-card-title>
             <v-card-text class="text-center" style="padding: 30px 0; height: 100%">
                 <p style=" font-size: 30px; "><v-icon size="37" style="margin-bottom: 3px;">mdi-cash</v-icon> Change Due</p>
