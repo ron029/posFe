93f968bb1ecb6d0ebb123b2b6625d5661d6e30ff
ron029 <bonifacioronmark@gmail.com>
2025-03-01 11:07:58 +0800

    define electron.js

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,15 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  publicPath: './', // Ensures assets are loaded using "./js/" instead of "js/"
+  outputDir: 'dist', // Keeps the built files in "dist"
+  assetsDir: 'assets', // Moves JS/CSS inside "dist/assets"
   transpileDependencies: [
     'vuetify'
   ],
   devServer: {
     port: 8080,
-    host: '0.0.0.0', // Allow access from any host
+    host: '127.0.0.1', // Allow access from any host
     hot: true,
-    liveReload: false,
-    allowedHosts: "all", // Allow all hosts
+    liveReload: true,
   },
 })
