db1e591c89e7542401805ef158f6b3221b6f9787
ron029 <bonifacioronmark@gmail.com>
2025-03-01 11:07:23 +0800

    config electron.js

---
+++ main.js
@@ -0,0 +1,26 @@
+const { app, BrowserWindow } = require("electron");
+
+let mainWindow;
+
+app.whenReady().then(() => {
+  mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  // Load from domain instead of local file
+  mainWindow.loadURL("http://4d-convenience-store.com/");
+
+  mainWindow.on("closed", () => {
+    mainWindow = null;
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
