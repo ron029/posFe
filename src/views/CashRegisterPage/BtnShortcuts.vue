db3cc9aea439d2e22a07dff7abbbdfdac10de126
ron029 <bonifacioronmark@gmail.com>
2025-03-05 01:38:05 +0800

    redesign instructions; add home shortcuts

--- src/views/CashRegisterPage/BtnShortcuts.vue
+++ src/views/CashRegisterPage/BtnShortcuts.vue
@@ -1,12 +1,26 @@
 <template>
-    <div>
-        <div style="height: 100px; width: 100px; background-color: blue; color: white; margin-bottom: 10px">F1 Sales</div>
-        <div style="height: 100px; width: 100px; background-color: blue; color: white; margin-bottom: 10px">F2 Inventory</div>
-        <div style="height: 100px; width: 100px; background-color: blue; color: white; margin-bottom: 10px">F6 Dashboard</div>
-        <div style="height: 100px; width: 100px; background-color: blue; color: white; margin-bottom: 10px">F7 Cancel</div>
-        <div style="height: 100px; width: 100px; background-color: blue; color: white; margin-bottom: 10px;">F8 Payment</div>
-        <div style="height: 100px; width: 100px; background-color: blue; color: white; margin-bottom: 10px">ALT + E Edit Item</div>
-        <div style="height: 100px; width: 100px; background-color: blue; color: white; margin-bottom: 10px">ALT + D Delete Item</div>
-        <div style="height: 100px; width: 100px; background-color: blue; color: white; margin-bottom: 10px">ALT + Q Edit QTY</div>
+    <div style="vertical-align: top" class="btnContainer">
+        <div style="height: 100px; width: 100px; background-color: blue; color: white; margin: 10px; display: inline-block; vertical-align: top;"><span>F1</span>Sales</div>
+        <div style="height: 100px; width: 100px; background-color: blue; color: white; margin: 10px; display: inline-block; vertical-align: top;"><span>F2</span>Inventory</div>
+        <div style="height: 100px; width: 100px; background-color: blue; color: white; margin: 10px; display: inline-block; vertical-align: top;"><span>F6</span>Dashboard</div>
+        <div style="height: 100px; width: 100px; background-color: orange; color: white; margin: 10px; display: inline-block; vertical-align: top;"><span>F7</span>Cancel</div>
+        <div style="height: 100px; width: 100px; background-color: green; color: white; margin: 10px; display: inline-block; vertical-align: top;;"><span>F8</span>Payment</div>
+        <br>
+        <div style="height: 100px; width: 100px; background-color: blue; color: white; margin: 10px; display: inline-block; vertical-align: top;"><span>ALT + E</span>Edit Item</div>
+        <div style="height: 100px; width: 100px; background-color: blue; color: white; margin: 10px; display: inline-block; vertical-align: top;"><span>ALT + D</span> DLT Item</div>
+        <div style="height: 100px; width: 100px; background-color: blue; color: white; margin: 10px; display: inline-block; vertical-align: top;"><span>ALT + Q</span>Edit QTY</div>
+        <div style="height: 100px; width: 100px; background-color: blue; color: white; margin: 10px; display: inline-block; vertical-align: top;"><span>ALT + P / P</span>Print</div>
     </div>
 </template>
+
+<style scoped>
+.btnContainer {
+    text-align: left;
+}
+.btnContainer div {
+    text-align: center;
+}
+.btnContainer div span {
+    display: block;
+}
+</style>
