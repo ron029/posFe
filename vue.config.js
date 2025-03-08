99bb3dffa3430a7d0631e893d05d9cd73c5262a3
ron029 <bonifacioronmark@gmail.com>
2025-03-08 13:45:51 +0800

    save last vuejs

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: './', // Ensures assets are loaded using "./js/" instead of "js/"
-  outputDir: 'dist', // Keeps the built files in "dist"
-  assetsDir: 'assets', // Moves JS/CSS inside "dist/assets"
   transpileDependencies: [
     'vuetify'
   ],
